47698afb54f6d9786cdca64950d0fa73bd5e7a73
Lucas De Castro Oliveira <lucas.co@bolsista.pti.org.br>
2017-10-05 11:14:56 -0300

    add right flags to chromium headless (karma.js)

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -100,7 +100,7 @@ module.exports = function (config) {
         customLaunchers: {
           ChromiumHeadless: {
             base: 'Chromium',
-            flags: ["--headless", "--no-sandbox", "--disable-gpu"]
+            flags: ["--headless --no-sandbox --disable-gpu"]
           }
         },
 
